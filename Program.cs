78da52f2243b85e2cf0b3818c09e4fac47809a01
steph <steph@DESKTOP-D0C09JV>
2022-09-20 11:23:08 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+
+namespace Deliverable_4_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            number = 25;
+            
+            int[] fib = new int[number];
+            fib[0] = 0;
+            fib[1] = 1;
+            
+            for (int i = 2; i < number; i++)
+            {
+                fib[i] = fib[i - 2] + fib[i - 1];
+            }
+
+            
+            foreach (int input in fib)
+            {
+                Console.WriteLine("Fibonacci number = " + input.ToString());
+            }
+                
+                
+        }
+    }
+}
